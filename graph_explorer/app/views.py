d059a5076818fa9a2468e09ffa09d0890e755129
aleksaqm <perovic.aleksa03@gmail.com>
2024-01-31 21:16:04 +0100

    feat: renamed previous plugin to use it as github data source plugin

--- graph_explorer/app/views.py
+++ graph_explorer/app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 import sys
 
 # sys.path.append(r'C:\Users\Vladimir\Desktop\prog\5semestar\sok\projekat\graph-expressiveness')
-sys.path.append(r'/home/dc/Code/SOK/graph_exp')
+sys.path.append(r'C:\Users\Aleksa\Desktop\sok-project\graph-expressiveness')
 from core.src.use_cases import load_plugins
 
 # from plugin.visualizer import SimpleVisualizer
@@ -17,7 +17,8 @@ def test(request):
     load_plugins.load_plugins()
     sources = load_plugins.get_source_plugins()
     visualizers = load_plugins.get_visualizer_plugins()
-    visualization_html = visualizers[0].show(sources[0].load(a))
+    graph = sources[0].load(a)
+    visualization_html = visualizers[0].show(graph)
 
     return render(request, 'index.html',
                   {"sources": sources, "visualizers": visualizers, "visualization_html": visualization_html})
