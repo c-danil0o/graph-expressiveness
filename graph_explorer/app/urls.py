6543de858c5b7fa786fe7589ef86b0f87360edf1
c-danil0o <danilo.cvijetic10@protonmail.ch>
2024-01-31 18:51:22 +0100

    refactor: refactored core structure to be modular #10

--- graph_explorer/app/urls.py
+++ graph_explorer/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test)
-    #path('simple/', views.test, name='show_graph')
+    path('', views.index, name='index_page'),
+    path('generate', views.generate, name='generated_graph')
+    # path('simple/', views.test, name='show_graph')
 ]
--- graph_explorer/app/views.py
+++ graph_explorer/app/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+import os
 import sys
 
-# sys.path.append(r'C:\Users\Vladimir\Desktop\prog\5semestar\sok\projekat\graph-expressiveness')
-sys.path.append(r'/home/dc/Code/SOK/graph_exp')
-from core.src.use_cases import load_plugins
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+sys.path.append(BASE_DIR)
 
-# from plugin.visualizer import SimpleVisualizer
-#
+from core.src.use_cases.loader import Loader
 
-a = {'blocks_num': 10, 'graph_name': 'asd', 'latest_block': -1}
+loader = Loader()
+loader.init()
 
 
-def test(request):
-    load_plugins.load_plugins()
-    sources = load_plugins.get_source_plugins()
-    visualizers = load_plugins.get_visualizer_plugins()
-    visualization_html = visualizers[0].show(sources[0].load(a))
-
-    return render(request, 'index.html',
-                  {"sources": sources, "visualizers": visualizers, "visualization_html": visualization_html})
-# def simple_visualizer_test():
-#     visualizer = SimpleVisualizer()
-#     generated_html = visualizer.show()
-#     return HttpResponse(generated_html)
+def index(request):
+    print(loader.sources)
+    return render(request, 'index.html', {"sources": loader.sources, "visualizers": loader.visualizers})
