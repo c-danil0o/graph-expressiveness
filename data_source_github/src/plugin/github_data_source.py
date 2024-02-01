aa8c1dccc0f0a5fc3a3a802d471788a5fc47004d
aleksaqm <perovic.aleksa03@gmail.com>
2024-02-01 18:51:35 +0100

    feat: Implemented github data source plugin #9

---
+++ data_source_github/src/plugin/github_data_source.py
@@ -0,0 +1,85 @@
+from api.src.services.source_plugin import SourcePlugin
+import typing
+from concurrent.futures import ThreadPoolExecutor
+from github import Github
+
+from api.src.types.graph import Graph, Node, Edge
+
+_graph = Graph('', None, [], [])
+access_token = "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"
+user_info_cache = {}
+
+new_users = []
+last_users = []
+
+
+def get_user_info(person):
+    name = person.login
+    if name in user_info_cache:
+        return user_info_cache[name]
+    user_info = {
+        "login": name,
+        "email": str(person.email),
+        "name": str(person.name),
+        "bio": str(person.bio)
+    }
+
+    user_info_cache[name] = user_info
+    return user_info
+
+
+def fetch_user_info_parallel(following_user, node):
+    user_info = get_user_info(following_user)
+    new_users.append(following_user)
+    new_node = Node(user_info["login"], {
+        "email": user_info["email"],
+        "name": user_info["name"],
+        "bio": user_info["bio"]
+    })
+    _graph.add_node(new_node)
+    _graph.add_edge(Edge("following", {}, node, new_node, False))
+
+
+def populate_graph(depth: int):
+    print('qm')
+
+    if depth == 0:
+        return _graph
+    else:
+        depth = depth-1
+        old_nodes = _graph.nodes.copy()
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for user in last_users:
+                for following_user in user.get_following():
+                    if _graph.get_node_count() >= 200:
+                        break
+                    futures.append(executor.submit(fetch_user_info_parallel, following_user, _graph.get_node(user.login)))
+            for future in futures:
+                future.result()
+        last_users.clear()
+        last_users.extend(new_users)
+        new_users.clear()
+        return populate_graph(depth)
+
+
+def load_graph(graph_name: str, depth: int) -> Graph:
+    login = Github(access_token)
+    user = login.get_user()
+    _graph.name = graph_name
+    root: Node = Node(user.login, {"email": str(user.email), "name": str(user.name), "bio": str(user.bio)})
+    _graph.set_root(root)
+    _graph.add_node(root)
+    last_users.append(user)
+    return populate_graph(depth)
+
+
+class DataSource(SourcePlugin):
+    def load(self, config: dict):
+        return load_graph(config['graph_name'], config["depth"])
+
+    def identifier(self):
+        return "graph-explorer-github-datasource"
+
+    def name(self):
+        return "github_datasource"
--- graph_explorer/app/views.py
+++ graph_explorer/app/views.py
@@ -10,7 +10,7 @@ from core.src.use_cases import load_plugins
 # from plugin.visualizer import SimpleVisualizer
 #
 
-a = {'blocks_num': 10, 'graph_name': 'asd', 'latest_block': -1}
+a = {'blocks_num': 10, 'graph_name': 'asd', 'latest_block': -1, 'depth': 2}
 
 
 def test(request):
@@ -19,7 +19,7 @@ def test(request):
     visualizers = load_plugins.get_visualizer_plugins()
     graph = sources[0].load(a)
     visualization_html = visualizers[0].show(graph)
-
+    # print(graph)
     return render(request, 'index.html',
                   {"sources": sources, "visualizers": visualizers, "visualization_html": visualization_html})
 # def simple_visualizer_test():
