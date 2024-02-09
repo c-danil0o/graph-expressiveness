1b8b98842bb08cd0e002f7377eaa74fa440e5420
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-02-09 19:13:58 +0100

    fix: fixed render functions to work with jinja2  #41

--- block_visualizer/src/plugin/block_visualizer.py
+++ block_visualizer/src/plugin/block_visualizer.py
@@ -1,7 +1,9 @@
+import os
 from urllib import request
 
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from jinja2 import Environment, FileSystemLoader
 
 from api.src.services.visualizer_plugin import VisualizerPlugin
 from api.src.types.graph import Graph, Node, Edge
@@ -28,5 +30,8 @@ class Visualizer(VisualizerPlugin):
             "links": edges,
         }
 
-        return render_to_string('block_visualization.html', context)
+        path = os.path.join(os.path.dirname(__file__))
+        jinja_env = Environment(loader=FileSystemLoader(path))
+        template = jinja_env.get_template('block_visualization.html')
+        return template.render(context)
 
